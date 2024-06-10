6e3ddfb932e9b0b98db28a04207dfe46d7a41c68
Himanshu Lohakare <hplohakare7@gmail.com>
2024-06-10 13:42:16 +0530

    code change

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
       const fetchWeatherByCity = async (city) => {
         try{
-          const apikey = process.env.API_KEY;
+          const apikey = '590dcedc5cb3c444a593854476b3b68e';
           const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`);
           setCoordinate({lon : response.data.coord.lon , lat : response.data.coord.lat});
           setWeather(response.data);
